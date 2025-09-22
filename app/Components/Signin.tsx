ec01231eaff3398f3cd990521ebabccf4582cd3c
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2025-09-22 16:36:34 +0200

    page de connexion

---
+++ app/Components/Signin.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function Signin() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    return (
+        <>
+        </>
+    );
+}
